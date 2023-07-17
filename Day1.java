28733a1ca0b9eb4131611379e9417601076d6483
Rita010 <ritakhatiwada50@gmail.com>
2023-07-17 14:06:53 -0700

    first commit

---
+++ Day1.java
@@ -0,0 +1,78 @@
+package SmritiMaamRevision;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		// write a program to check number is odd/even
+		
+		
+		
+			//	 Write a program to check number is odd or even
+			
+			
+		
+			// Parogram 1
+			
+		
+			System.out.println(10%2); // modulus 0, even
+			System.out.println(23%5); // modulus 3 , odd
+			
+	//	
+//		 	if(condition) {
+//			(block)
+//				}
+//			else {
+//			
+//			}
+
+		
+		// Program 2
+		
+			
+			
+		int q1 = 7;
+		if(q1%2 == 0) {
+			  System.out.println("even");    
+		}
+		else {
+			System.out.println("odd");
+		}
+		
+		
+		// Program 3
+		
+		
+		
+		int a = 5;
+		if(a%2 == 0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+		
+		
+		
+		// Program 4
+		
+		
+		int b = 10;
+		if (b%2 == 0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+		
+		
+		
+		
+		
+		
+		
+	
+		
+
+	}
+
+}
